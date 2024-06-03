8986ed173158eb53814e0f651788088861818fda
sudip <sudeeplamichhane18@gmail.com>
2024-06-03 16:35:21 +0545

    Add services serction

--- app/page.js
+++ app/page.js
@@ -5,7 +5,25 @@ export default function Home() {
   return (
     <>
       <div className="homepage-first">
-        <Image className="homepage-coverpic" width={1000} height={350} src="https://images.unsplash.com/photo-1676311396794-f14881e9daaa?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="sahayata cover" />
+        <div className="homepage-coverpic-wrapper">
+          <Image className="homepage-coverpic" width={1000} height={350} src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAGLgko.img" alt="sahayata cover" />
+        </div>
+        <div className="homepage-slogan">
+          Quick Home Fixes At your Fingertips
+        </div>
+      </div>
+      <div className="homepage-second">
+        <h2 className="homepage-second-title">Book Services</h2>
+        <div className="service-wrapper">
+          <ul>
+            <li>Electrician</li>
+            <li>Plumber</li>
+            <li>oassis</li>
+            <li>maddath</li>
+            <li>sudip</li>
+            <li>khajado</li>
+          </ul>
+        </div>
       </div>
     </>
   );
