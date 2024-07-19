8b6c1c6cfdb728903ac32e53592823b412324710
Riton Charlox <ritoncharlox25@gmail.com>
2024-07-19 14:20:04 +0545

    updated profile page

--- app/profile/page.js
+++ app/profile/page.js
@@ -8,6 +8,7 @@ import { FaUser, FaUserCircle } from "react-icons/fa";
 import { FaGear } from "react-icons/fa6";
 import Image from 'next/image';
 import { FaEdit } from "react-icons/fa";
+import Profile from '@/components/Profile/Profile';
 
 export default async function ProfilePage() {
   const session = await auth();
@@ -28,60 +29,20 @@ export default async function ProfilePage() {
 
   const role = user.isAdmin ? "Admin" : user.isFreelancer ? "Freelancer" : "User";
 
+  const handleAvatarChange = async ( user, avatarUrl ) => {
+    "use server"
+
+    console.log(user, avatarUrl);
+    return "Hello";
+  }
+
+  const data = {
+    user: user,
+    role: role,
+    handleAvatarChange: handleAvatarChange,
+  }
+
   return (
-    <main className="profile">
-      <div className="profile-section">
-        <div className="sidebar">
-          <div className="sidebar-item">
-            <div className="icon">
-              <FaUser />
-            </div>
-            <div className="text">
-              Personal Details
-            </div>
-          </div>
-          <div className="sidebar-item">
-            <div className="icon">
-              <FaGear />
-            </div>
-            <div className="text">
-              Settings
-            </div>
-          </div>
-        </div>
-        <div className="line"></div>
-        <div className="content">
-          <div className="content-heading">
-            Personal Details
-          </div>
-          <div className="user-card">
-            <div className="profile-avatar">
-              {
-                user?.avatar ?
-                  // <Image src={user?.avatar} width={30} height={30} quality={100} unoptimized alt="User Avatar" className="avatar-image" />
-                  <Image className="profile-avatar-image" width={100} height={100} src={user.avatar} alt="sahayata cover" />
-                  :
-                  <div className="profile-avatar-image-alt">
-                    <FaUserCircle />
-                  </div>
-              }
-              <div className="profile-avatar-edit-button">
-              <FaEdit />
-              </div>
-            </div>
-            <div className="name">
-              {
-                user.name
-              }
-            </div>
-            <div className="role">
-              {
-                role
-              }
-            </div>
-          </div>
-        </div>
-      </div>
-    </main>
+    <Profile data={data} />
   );
 }
