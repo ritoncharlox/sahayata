8ff403f7e2e225a10092c8786e1e0fb0ecd74c8d
Riton Charlox <ritoncharlox25@gmail.com>
2024-07-30 23:06:57 +0545

    updated dashboard

---
+++ app/dashboard/_components/AdminDashboard/Users/Users.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Users.css';
+
+const Users = () => {
+
+    return (
+        <>
+            <h1>Users</h1>
+            <p>Manage users here.</p>
+            {/* Add more users content here */}
+        </>
+    );
+};
+
+export default Users
