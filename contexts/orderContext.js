9f7965e176f36a7e9d591d2e958f65a8a7d8d033
sudip <sudeeplamichhane18@gmail.com>
2024-08-01 09:45:49 +0545

    updated order popup

--- contexts/orderContext.js
+++ contexts/orderContext.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 const OrderContext = createContext();
@@ -11,23 +11,51 @@ export default function OrderProvider({ children }) {
 
     const [orders, setOrders] = useState([]);
 
+    useEffect(() => {
+      let myOrders = localStorage.getItem("order");
+      const newOrders = JSON.parse(myOrders);
+      if(myOrders){
+        setOrders(newOrders);
+      }
+    }, [])
+
+    const saveToLs = (ordersToSave)=>{
+        localStorage.setItem("order", JSON.stringify(ordersToSave));
+    }
+    const RemoveFromLs = ()=>{
+        localStorage.removeItem("order");
+    }
+
     const addOrder = (order) => {
         const id = uuidv4();
-        setOrders((prevOrders) => [
-            ...prevOrders,
-            {
-                ...order,
-                id: id
-            }
-        ]);
+        // setOrders((prevOrders) => [
+        //     ...prevOrders,
+        //     {
+        //         ...order,
+        //         id: id
+        //     }
+        // ]);
+        let newOrders = [...orders];
+        order.id = id;
+        newOrders.push(order);
+        setOrders(newOrders)
+        saveToLs(newOrders);
+        
     };
 
     const removeOrder = (id) => {
-        setOrders((prevOrders) => prevOrders.filter((order) => order.id !== id));
+        // setOrders((prevOrders) => prevOrders.filter((order) => order.id !== id));
+        let newOrders = [...orders];
+        let reaminingOrders = newOrders.filter((item)=>{
+            return item.id !== id;
+        })
+        setOrders(reaminingOrders);
+        saveToLs(reaminingOrders);;
     };
 
     const cancelOrder = ()=>{
         setOrders([]);
+        RemoveFromLs();
     }
 
     return (
